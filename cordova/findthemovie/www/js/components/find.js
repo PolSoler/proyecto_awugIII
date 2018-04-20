c6722d67a5d0caea94183c55e8ba1a015dc81d32
Marc Masip <marcmc421@gmail.com>
2018-04-20 02:44:27 +0200

    bottom bar with router, components, moviedb popular movies

--- cordova/findthemovie/www/js/components/find.js
+++ cordova/findthemovie/www/js/components/find.js
@@ -1,14 +1,61 @@
-const FindTemplate = {props: [], 
-        data: () => ({ }),
-        methods: { },
+const FindTemplate = {props: {}, 
+        data(){
+          return{
+            type: null,
+            movies:[]
+          }
+        },
+        methods:{
+          getMoviesListAndDrawList: function(){//load list by popularity
+              var theList = $("#popularlist");
+              var mo=[];
+               var request = $.ajax({
+                    url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+moviedbapi,
+                    method: "GET"
+                  });
+                  request.done(function( moviesList ) {
+                      for (i=0;i<9;i++){
+                           /* theList.append( `<md-card md-with-hover>
+                                  <md-ripple>
+                                    <md-card-media>
+                                      <img class='poster' src='http://image.tmdb.org/t/p/w342//` + moviesList.results[i].poster_path + `'>
+                                    </md-card-media>
+
+                                    <md-card-header>
+                                      <div class="md-title">` + moviesList.results[i].original_title + `</div>
+                                    </md-card-header>
+                                </md-ripple>
+                              </md-card>
+                              `);*/
+                              //Vue.set(movies[i], 'test','hello');
+                              mo.push({"poster":"http://image.tmdb.org/t/p/w342//"+moviesList.results[i].poster_path, 'title':moviesList.results[i].original_title});
+                               //this.$set(this.movies[i], 'poster', "http://image.tmdb.org/t/p/w342//"+moviesList.results[i].poster_path);
+                               //this.$set(this.movies[i], 'title', moviesList.results[i].original_title);
+                          }
+                      });
+                  request.fail(function( jqXHR, textStatus ) {
+                    alert( "Request failed: " + textStatus );
+                  });
+                  this.movies=mo;
+              }
+              
+        },
+        created: function () {
+          this.getMoviesListAndDrawList();
+        },
         template:`
         <div>
             <div id="main">
-                <h1 class="md-title">Find<br>The<br>Movie</h1>
+                <h1 class="md-title maintitle">Find<br>The<br>Movie</h1>
+                <div id="searchfield">
                 <md-field>
-                  <label>Find by text</label>
+                  <label>Find movie by text</label>
                   <md-input v-model="type"></md-input>
                 </md-field>
+                <md-button class="md-icon-button md-dense md-primary search">
+                  <md-icon>search</md-icon>
+                </md-button>
+                </div>
                 <div id="fimage">
                   <md-button class="md-fab md-primary">
                      <md-icon>camera_alt</md-icon>
@@ -16,7 +63,25 @@ const FindTemplate = {props: [],
                   <br>
                   <small>Find by image</small>
                 </div>
+                <div id='popularmovies'>
+                  <p>Popular Movies</p>
+                  <div id="popularlist">
+                      <md-card md-with-hover v-for="movie in movies" >
+                          <md-ripple>
+                            <md-card-media>
+                              <img class='poster' :src='movie.poster'>
+                            </md-card-media>
+
+                            <md-card-header>
+                              <div class="md-title">{{movie.title}}</div>
+                            </md-card-header>
+                        </md-ripple>
+                      </md-card>
+                  </div>
+                </div>
             </div>     
         </div>
 `
                      };
+
+/**/
--- cordova/findthemovie/www/js/main.js
+++ cordova/findthemovie/www/js/main.js
@@ -1,25 +1,23 @@
     Vue.use(VueMaterial.default);
     Vue.use(VueRouter);
+ /*   Vue.material.registerTheme('principal', {
+          primary: {
+            color: 'orange',
+            hue: 'A200'
+          },
+          accent: {
+            color: 'grey',
+            hue: 'A200'
+          }
+        });
 
-//    Vue.material.registerTheme('about', {
-//          primary: {
-//            color: 'indigo',
-//            hue: 'A200'
-//          },
-//          accent: {
-//            color: 'grey',
-//            hue: 300
-//          }
-//        });
-
-//Vue.material.setCurrentTheme('about');
+Vue.material.setCurrentTheme('principal');*/
 
 
 function init(){
     
     
     const routes = [
-                //{path: '/inbox', name: 'inbox', component: MailListTemplate},
                 {path: '/find', name: 'find',  component: FindTemplate}
             ,{path: '/settings', name: 'settings',  component: SettingsTemplate}
             ,{path: '/simplelist', name: 'simplelist', component: SimpleListTemplate}
@@ -34,7 +32,8 @@ function init(){
         router,
         data: { showNavigation: false,
                 showSidepanel: false,
-              message: 'Hola!'},
+                movies: []
+            },
         methods: {
             goToFind: function(){
                 this.showNavigation = false;
@@ -50,14 +49,49 @@ function init(){
                 this.showNavigation = false;
                     //this.$refs.sidebar.toggle();
                     router.push('simplelist');
-                }
-        }
+                },
+            
+        },
+        components: { 'find': FindTemplate }
+        
             
       }).$mount('#app');
     
     //router.push('settings');
     router.push({ name: 'find'})
+    //FindTemplate.getMoviesListAndDrawList();
 }
 
+/*function {//load list by popularity
+    var theList = $("#popularlist");
+    
+     var request = $.ajax({
+          url: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+moviedbapi,
+          method: "GET"
+        });
+
+        request.done(function( moviesList ) {
+            
+            for (i=0;i<9;i++){
+                  theList.append( `<md-card md-with-hover>
+                        <md-ripple>
+                          <md-card-media>
+                            <img class='poster' src='http://image.tmdb.org/t/p/w342//` + moviesList.results[i].poster_path + `'>
+                          </md-card-media>
+
+                          <md-card-header>
+                            <div class="md-title">` + moviesList.results[i].original_title + `</div>
+                          </md-card-header>
+                      </md-ripple>
+                    </md-card>
+                    `);
+                    
+                }
+            
+            });
+        request.fail(function( jqXHR, textStatus ) {
+          alert( "Request failed: " + textStatus );
+    });
+}*/
         
 
